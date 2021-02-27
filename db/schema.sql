7801ae45887906e83a841a48fc450ffd2d1f2708
Johann Taylor <j.taylor1343@gmail.com>
2021-02-27 15:39:47 -0500

    Added display all departments

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employee_DB;
+CREATE DATABASE employee_DB;
+USE employee_DB;
+
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
+
+CREATE TABLE department (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL(10,2) NOT NULL,
+department_id INT NOT NULL,
+foreign key(department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
+foreign key(role_id) REFERENCES role(id),
+manager_id INT,
+foreign key(manager_id) REFERENCES employee(id)
+);
---
+++ server.js
@@ -0,0 +1,104 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+require('console.table');
+
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'P@$$word',
+  database: 'employee_DB'
+});
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log('connected as id ' + connection.threadId);
+  promptUser();
+});
+
+// function listEmployees(){
+//     connection.query
+// }
+
+
+function promptUser(){
+    console.log('\n',
+        `
+        ====================
+
+        ====================
+        `,
+        '\n'
+    );
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'option',
+            message: 'What would you like to do?',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Exit']
+        }
+    ])
+    .then(answer => {
+        if(answer.option === 'View all departments') { viewDepartments(); }
+        if(answer.option === 'View all roles') { viewRoles(); }
+        if(answer.option === 'View all employees') { viewAllEmployees(); }
+        if(answer.option === 'Add a department') { console.log(answer); }
+        if(answer.option === 'Add a role') { console.log(answer); }
+        if(answer.option === 'Add an employee') { addEmployee(); }
+        if(answer.option === 'Update an employee role') { }
+        if(answer.option === 'Exit') { connection.end(); }
+    })
+}
+
+
+viewDepartments = () => {
+    console.log('View departments');
+    connection.query(`SELECT id AS department_id, name AS department_name FROM department;`, function(err, res) {
+    if(err) throw err;
+    console.table('\n', res, '\n');
+    promptUser();
+  })
+};
+
+viewAllEmployees = () => {
+    connection.query(`SELECT * FROM employee LEFT JOIN role ON employee.role_id = role.id;`, function(err, res) {
+        if(err) throw err;
+        console.table('\n', res, '\n');
+        connection.end();
+    })
+}
+
+viewRoles = () => {
+    connection.query(`SELECT * FROM role`, function(err, res) {
+        if(err) throw err;
+        console.table('\n', res, '\n');
+        connection.end();
+    })
+}
+
+addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: "Enter the employee's first name",
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: "Enter the employee's first name",
+        },
+        {
+            type: 'input',
+            name: 'role_id',
+            message: "Enter the employee's first name",
+        }
+    ])
+    console.log('Add employee');
+    connection.query(`INSERT INTO * employee (first_name, last_name, role_id, manager_id)`, function(err, res) {
+    if(err) throw err;
+    console.log(res)
+    connection.end();
+  })
+};
