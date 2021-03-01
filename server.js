f9aa61170cb8afc4dd6913060b808484192d9328
Johann Taylor <j.taylor1343@gmail.com>
2021-02-28 21:54:22 -0500

    Added update employee role

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ connection.connect(err => {
 });
 
 
-
+//===== PROMPT USER QUESTIONS =====//
 function promptUser() {
     inquirer.prompt([{
             type: 'list',
@@ -54,7 +54,9 @@ function promptUser() {
         })
 }
 
+//============================== SECTION TO VIEW TABLES ==============================//
 
+//===== VIEW ALL DEPARTMENTS =====//
 viewDepartments = () => {
     console.log('View departments');
     connection.query(`SELECT id AS department_id, name AS department_name FROM department;`, function (err, res) {
@@ -64,6 +66,7 @@ viewDepartments = () => {
     })
 };
 
+//===== VIEW ALL EMPLOYEES =====//
 viewAllEmployees = () => {
     connection.query(`
     SELECT employee.id, employee.first_name, employee.last_name, title, department.name AS department, salary, 
@@ -81,6 +84,7 @@ viewAllEmployees = () => {
     })
 }
 
+//===== VIEW ALL ROLES =====//
 viewRoles = () => {
     connection.query(`
     SELECT title, role_id, department.name, salary FROM role
@@ -94,7 +98,10 @@ viewRoles = () => {
             promptUser();
         })
 }
+//============================== END OF VIEW SECTION ==============================//
 
+
+//========= ADD DEPARTMENT ========//
 addDepartment = () => {
     inquirer.prompt({
         type: 'input',
@@ -114,6 +121,7 @@ addDepartment = () => {
     })
 }
 
+//======= ADD ROLE =======//
 addRole = () => {
     connection.query('SELECT title FROM role;', function (error, res) {
         if (error) throw error;
@@ -156,6 +164,8 @@ addRole = () => {
     })
 }
 
+
+//=========== ADD EMPLOYEE ===========//
 addEmployee = () => {
     inquirer.prompt([{
                 type: 'input',
@@ -191,33 +201,44 @@ addEmployee = () => {
 
 };
 
-
+//========== UPDATE EMPLOYEE ROLE =========//
 updateEmployeeRole = () => {
-    viewAllEmployees();
-
     connection.query(`
-    SELECT CONCAT(first_name, ' ', last_name)
-    AS employee
-    FROM employee;`, function (err, res) {
+    SELECT employee.id, employee.first_name, employee.last_name, title, department.name AS department, salary, 
+    CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee 
+    LEFT JOIN role 
+    ON employee.role_id = role.id
+	LEFT JOIN department
+	ON role.department_id = department.id
+    LEFT JOIN employee manager
+    ON manager.id = employee.manager_id;`, (err, res) => {
         if (err) throw err;
-        const employeeList = res.map(({
-            employee
-        }) => ({
-            name: employee
-        }));
-        inquirer
-            .prompt([{
-                    type: 'list',
-                    name: 'employeeName',
-                    message: 'Select an employee to update',
-                    choices: employeeList
-                },
+        console.table('\n', res, '\n');
+        }
+    );
 
-            ])
-            .then(data => {
-                connection.query(`
-                UPDATE employee SET role_id =
-                `)
-            })
-    })
+    inquirer
+        .prompt([{
+                type: 'input',
+                name: 'employeeID',
+                message: "Enter the employee's ID you would like to update"
+            },
+            {
+                type: 'input',
+                name: 'newRoleID',
+                message: 'Enter a new role ID for the employee',
+            }
+        ])
+        .then(data => {
+            connection.query(`
+                UPDATE employee 
+                SET role_id = ${data.newRoleID} 
+                WHERE id = ${data.employeeID};
+                `, function (err) {
+                    if (err) throw err;
+                    viewAllEmployees();
+                    promptUser();
+                })
+        })
 }
